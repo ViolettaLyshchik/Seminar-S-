b6b83220cfecb7872be4c7f0fbfd300117638b9d
Violetta <viola.kotovich@gmail.com>
2022-06-27 23:59:36 +0300

    task1.квадрат числа

---
+++ Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Проверим, является ли первое чисто квадратом второго. Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+if (second * second == first)
+{
+    Console.WriteLine($"Да, первое число, является квадратом второго {second} * {second} = {first}");
+}
+else
+{
+    Console.WriteLine($"Нет, первое число не является квадратом второго {second} * {second} != {first}");
+}
+
