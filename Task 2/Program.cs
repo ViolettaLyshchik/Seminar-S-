a9699acc3e4ac40b4aa9693fa8bd245e6f8b1125
Violetta <viola.kotovich@gmail.com>
2022-06-28 17:37:22 +0300

    task2 ДЗ больше меньше

---
+++ Task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2 программа принимает на вход 2 числа, и выдает какое больше,какое меньше
+
+Console.Write("Проверим, какое число больше, а какое меньше. Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+if (first > second)
+{
+    Console.WriteLine($"Число {first}, больше числа {second} :{first} > {second}");
+}
+else
+{
+    Console.WriteLine($"Число {second} больше числа {first} :{second} > {first}");
+}
