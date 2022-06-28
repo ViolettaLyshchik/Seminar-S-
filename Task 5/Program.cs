e606dfcb3df5df911280ae7e4632bdcf9d91a1c1
Violetta <viola.kotovich@gmail.com>
2022-06-28 10:09:59 +0300

    task5 Целое число от -N до N

---
+++ Task 5/Program.cs
@@ -0,0 +1,7 @@
+﻿//Целые числа от -N до N
+Console.Write ("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int index = -number; index <= number; index++)
+{
+    Console.Write($"{index} ");
+}
