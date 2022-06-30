ed5862fd050b774fd338e3c743982f5ea6e7f615
Violetta <viola.kotovich@gmail.com>
2022-06-30 21:39:44 +0300

    Task 13 дз 3 цифра любого числа

---
+++ Task 13/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5'-'
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.WriteLine("Программа выдает третью цифру числа введенного пользователем");
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num < 100)
+{
+    Console.Write("Tретьей цифры нет. Введите другое  число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(num > 99 ? num.ToString()[2] : '-');
