efec75193c8750154722744bf7778b355921e3d9
Violetta <viola.kotovich@gmail.com>
2022-06-30 14:13:38 +0300

    Task 9 наибольшее число из отрезка

---
+++ Task 9/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу,которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа
+
+int rndNum = new Random().Next(10,100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {rndNum}");
+
+int firstDigit = rndNum / 10;
+int secondDigit = rndNum % 10;
+int maxDigit = 0;
+
+if(firstDigit > secondDigit) maxDigit = firstDigit;
+else maxDigit = secondDigit;
+
+Console.WriteLine($"Наибольшая цифра числа {rndNum} => {maxDigit}");
+
