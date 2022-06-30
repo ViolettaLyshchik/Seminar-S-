3ee729ba477b32b47c233308bb874df60c783ab5
Violetta <viola.kotovich@gmail.com>
2022-06-30 15:16:15 +0300

    Task 11 удаление с трехзначного числа второй цифры

---
+++ Task 11/Program.cs
@@ -0,0 +1,18 @@
+﻿/* 11. напишите программу, которое выводит случайное трехзначное число и удаляет вторую цифру этого числа
+456->46
+782->72*/
+
+Console.WriteLine("Программа выдает  цифру трехзачного числа введенного пользователем");
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while ( num < 100 || num > 999)
+{
+    Console.Write("Введено не трехзначное число. Введите трехзначное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+ int firstDigit = (num / 100) * 10;      //или в одну строку
+ int thirdDidit = (num % 10);            //записать
+ int result = firstDigit + thirdDidit;   //
+ Console.WriteLine($"Полученное двухзначное число: {result}");              //Console.WritLine(nam / 100 * 10 + num % 10);
+
+ 
