dfefe2d0ef09b438777d8a7816d9384505820cd1
Violetta <viola.kotovich@gmail.com>
2022-07-01 10:44:01 +0300

    Task 16 Является ли одно число квадратом другого

---
+++ Task 16/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Проверим, является ли одно число квадратом другого. Введите первое число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+if (firstNum * firstNum == secondNum | secondNum * secondNum == firstNum)
+{
+    Console.Write("Одно из чисел является квадратом другого.");
+}
+
+else
+{
+    Console.Write("Ни одно из чисел не является квадратом другого.");
+}
