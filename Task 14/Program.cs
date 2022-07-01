eb1546c43d75c80f7736d902fd6973f699df8302
Violetta <viola.kotovich@gmail.com>
2022-07-01 11:03:57 +0300

    Task 14 кратно ли число одновременно  7 и 23

---
+++ Task 14/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Кратно ли число 7 и 23.Введите число:");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.Write($"{num} кратно и 7, и 23");
+
+}
+else
+{
+    Console.Write($"{num} не кратно 7 и 23. ");
+}
