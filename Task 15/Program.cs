fd0fcdaa242b884f81d85f57298ffd7fb42017cf
Violetta <viola.kotovich@gmail.com>
2022-07-01 12:08:19 +0300

    Task 15 дз выходной или рабочий день

---
+++ Task 15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Проверим рабочий или выходной день. Введите число от 1 до 7:  ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+while (dayNum > 7)
+{
+    Console.Write("Введено число не соответствующее дню недели. Введите число от 1 до 7: ");
+    dayNum = Convert.ToInt32(Console.ReadLine());
+}
+
+if (dayNum >= 1 && dayNum <= 5)
+{
+    Console.Write("Рабочий день");
+}
+else
+{
+    Console.Write(" Выходной ");
+}
