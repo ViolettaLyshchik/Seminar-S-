247b9e8b4186ad91d5135a3a61514abb4945814e
Violetta <viola.kotovich@gmail.com>
2022-07-04 12:22:17 +0300

    Task 22 таблица квадратов чисел от 1 до N

---
+++ Task 22/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+5-> 1,4,9,16,25
+2-> 1,4*/
+
+Console.WriteLine(" Введите число для вычесления набора квадратов чисел");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+int square;
+while (count <= num)
+{
+    square = count * count;
+    Console.WriteLine($"{count,5} -> {square, 5} ");
+    count++;
+}
