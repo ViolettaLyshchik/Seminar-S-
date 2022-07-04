e28b1b8a8bf2236736976920a1d03bef99ed7f18
Violetta <viola.kotovich@gmail.com>
2022-07-04 19:09:33 +0300

    Task 19 ДЗ Палиндом ли число

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -6,17 +6,17 @@
 Console.WriteLine("Программа проверяет, является ли число палиндромом");
 Console.Write("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-while ( number < 10000 || number > 99999)
+while (number < 10000 || number > 99999)
 {
     Console.Write("Введено не пятизначное число. Введите пятизначное  число: ");
     number = Convert.ToInt32(Console.ReadLine());
 }
-int pal = 0 ;
+int pal = 0;
 int num = number;
 
-while (number > 0) 
+while (number > 0)
 {
-    pal = pal * 10 + number % 10; 
-    number/= 10;
+    pal = pal * 10 + number % 10;
+    number /= 10;
 }
- Console.WriteLine(num == pal ? "число является палиндромом " : "число не является полиндромом палиндром");
+Console.WriteLine(num == pal ? "число является палиндромом " : "число не является палиндромом ");
