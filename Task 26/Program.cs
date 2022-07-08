3d7efdf1e03d700c8ffd01424bba56b9cfab0af6
Violetta <viola.kotovich@gmail.com>
2022-07-08 12:48:52 +0300

    task 26 Кол-во цифр в числе

---
+++ Task 26/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 26 Написать программу, которая принимает на вход число и выдает количество цифр в числе*/
+
+Console.WriteLine("Данная программа покажет количество цифр в числе. Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int DigitsCount (int a )
+{
+    int digitsCounter = 0;
+    while(a !=0)
+    {
+        a /= 10;
+        digitsCounter++;
+    }
+    return digitsCounter;
+}
+int amoundOfDigits = DigitsCount(num);
+Console.Write($" Количество цифр в числе {num}  = {amoundOfDigits}");
