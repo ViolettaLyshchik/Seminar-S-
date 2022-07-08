f878deaf0df100837a5763092049656dacaa0cfd
Violetta <viola.kotovich@gmail.com>
2022-07-08 13:24:48 +0300

    task 28 Произведение чисел от 1 до N

---
+++ Task 28/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу которая принимает на вход число N и выдает произведение чисел от 1 до N
+
+Console.WriteLine("Произведение чисел от 1 до N. Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Prodact(int num)
+{
+    int prodact = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        prodact = prodact * i;
+    }
+    return prodact;
+}
+
+int res = Prodact(n);
+Console.WriteLine($"Произведение чисел от 1 до {n} = {res}");
