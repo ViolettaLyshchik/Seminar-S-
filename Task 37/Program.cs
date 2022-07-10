077e0d6490b37a9cc91d6708091196cd78f908e0
Violetta <viola.kotovich@gmail.com>
2022-07-10 20:43:09 +0300

    task 37 Произведение пар чисел в одномерном массиве

---
+++ Task 37/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Задача 37: Найдите произведение пар чисел в одномерном массиве.
+Парой считаем первый и последний элемент, второй и предпоследний
+и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21*/
+
+int[] CreateArrayRndInt(int size, int begin, int end)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(begin, end + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"Сгенерированный массив из {array.Length} элементов: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write($"{array[i]} , ");
+        else Console.WriteLine($"{array[i]} ]");
+    }
+}
+
+int[] MulArrElem(int[] array)
+{
+    int len = 0;
+    if (array.Length % 2 == 0) len = array.Length / 2;
+    else len = array.Length / 2 + 1;
+    int[] ResArr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        ResArr[i] = array[i] * array[array.Length - 1 - i];
+    }
+    if (len % 2 != 0) ResArr[len - 1] = array[len - 1];
+    return ResArr;
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное число диапазона: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число диапазона: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[] genArr = CreateArrayRndInt(size, min, max);
+int[] resArr = MulArrElem(genArr);
+PrintArray(genArr);
+PrintArray(resArr);
