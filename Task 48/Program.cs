d575a5e05be33771b8c830f36012cfdb38704718
Violetta <viola.kotovich@gmail.com>
2022-07-16 15:47:14 +0300

    task 48 Задать 2 мерный массив наполненный элементами по формуле

---
+++ Task 48/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 48: Задайте двумерный массив размера m на n,
+каждый элемент в массиве находится по формуле: Amn = m+n.
+Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5*/
+
+
+Console.Clear();
+int[,] MatrixOfNotRnd(int lines, int col)
+{
+    int[,] matrix = new int[lines, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i+j;
+        }
+    }
+    return matrix;
+}
+void PrintArrMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write(array[i, j] + ",");
+            else Console.Write(array[i, j] + "]");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] arrayResult = MatrixOfNotRnd(3, 4);
+PrintArrMatrix(arrayResult);
