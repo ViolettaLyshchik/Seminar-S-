086884ed2612ae372bb1d5afa49cad539f0b151d
Violetta <viola.kotovich@gmail.com>
2022-07-16 16:39:53 +0300

    task 49 замена елемнтов в двумерном массиве на их квадраты

---
+++ Task 49/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+Console.Write("Введите количество строк: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение диапазона: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение диапазона: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] GenMatrixRnd(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write(array[i, j] + ",");
+            else Console.Write(array[i, j] + "]");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] Sqrt(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j+=2)
+        {
+            array[i,j]*=array[i,j];
+        }
+       
+    }
+    return array;
+}
+int[,] genArray= GenMatrixRnd(lines,col,min,max);
+PrintMatrix(genArray);
+Console.WriteLine();
+int[,] arrayResult = Sqrt(genArray);
+PrintMatrix(arrayResult);
