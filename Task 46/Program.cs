873d00c691cce9d09948689badc33c032221b920
Violetta <viola.kotovich@gmail.com>
2022-07-16 14:55:55 +0300

    task 46 задать двумерный массив

---
+++ Task 46/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 46.Задать двумерный массив 
+
+
+
+
+
+
+Console.Clear();
+int[,] GenMatrixRnd(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write(array[i, j] + ",");
+            else Console.Write(array[i, j] + "]");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] arrayResult = GenMatrixRnd(3, 4, 10, 30);
+PrintMatrix(arrayResult);
