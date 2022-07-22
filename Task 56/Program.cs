a14521ec7a2d542c75ae1d0aba5805779d8d7f25
Violetta <viola.kotovich@gmail.com>
2022-07-22 10:36:24 +0300

    task 56 дз номер строки с наимеьшей суммой элементов

---
+++ Task 56/Program.cs
@@ -0,0 +1,67 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+Console.Clear();
+int[,] GenMatrixRnd(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write(array[i, j] + " , ");
+            else Console.Write(array[i, j] + "]");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int col = Convert.ToInt32(Console.ReadLine());
+int[,] array = GenMatrixRnd(row, col, 1, 10);
+PrintMatrix(array);
+Console.WriteLine();
+
+
+int SumLineElem(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+int minSumLine = 0;
+int sumLine = SumLineElem(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+Console.WriteLine($"Наименьшая сумма елементов:{sumLine} ,находится в {minSumLine+1} строке");
+
+
