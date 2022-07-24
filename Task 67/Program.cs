1868f7f139059c703394b2590daea0b0827b263d
Violetta <viola.kotovich@gmail.com>
2022-07-24 12:26:25 +0300

    task 67 возведение в степень. рекурсия

---
+++ Task 67/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 67: Напишите программу, которая будет принимать на вход число и
+возвращать сумму его цифр.
+453 -> 12
+45 -> 9*/
+Console.Clear();
+Console.Write("Задайте число : ");
+int num = int.Parse(Console.ReadLine());
+int sum = SumNum(num, 0);
+Console.WriteLine(sum);
+
+
+int SumNum(int num, int sum)
+{
+    sum += num % 10;
+    if (num / 10 == 0) return sum;
+    else return SumNum(num / 10, sum);
+}
